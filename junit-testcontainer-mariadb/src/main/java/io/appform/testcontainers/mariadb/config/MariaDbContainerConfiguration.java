b1483ebd7dcc56c1b781aa78d10798af24dd5e34
Ravi Desai <ravidesai47@gmail.com>
2021-07-30 20:01:40 +0530

    Updating mariadb config to be in sync with other configs

--- junit-testcontainer-mariadb/src/main/java/io/appform/testcontainers/mariadb/config/MariaDbContainerConfiguration.java
+++ junit-testcontainer-mariadb/src/main/java/io/appform/testcontainers/mariadb/config/MariaDbContainerConfiguration.java
@@ -1,48 +1,25 @@
 package io.appform.testcontainers.mariadb.config;
 
 import io.appform.testcontainers.commons.CommonContainerConfiguration;
-import lombok.Builder;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-import org.apache.commons.lang3.ObjectUtils;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 @ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true)
 public class MariaDbContainerConfiguration extends CommonContainerConfiguration {
 
     public static final int DEFAULT_PORT = 3306;
+    private static final String MARIADB = "mariadb";
 
-    private String defaultDockerImage = "mariadb:10.6.3-focal";
-    private String defaultDatabaseName = "test";
-    private String defaultUsername = "mariadb";
-    private String defaultPassword = "mariadb";
-
-    private String dockerImage = defaultDockerImage;
-
-    private String databaseName = defaultDatabaseName;
-
-    private String userName = defaultUsername;
-
-    private String userPassword = defaultPassword;
-
-    private String rootPassword = defaultPassword;
-
-    @Builder(builderMethodName = "mariadbBuilder", builderClassName = "MariaDbContainerConfigurationBuilder")
-    public MariaDbContainerConfiguration(Long waitTimeoutInSeconds, Boolean enabled,
-        String dockerImage, String databaseName, String userName, String userPassword,
-        String rootPassword) {
-        super(
-            ObjectUtils.defaultIfNull(waitTimeoutInSeconds,
-                CommonContainerConfiguration.DEFAULT_WAIT_TIMEOUT_IN_SECONDS),
-            ObjectUtils.defaultIfNull(enabled, CommonContainerConfiguration.DEFAULT_IS_ENABLED));
-        this.dockerImage = ObjectUtils.defaultIfNull(dockerImage, defaultDockerImage);
-        this.databaseName = ObjectUtils.defaultIfNull(databaseName, defaultDatabaseName);
-        this.userName = ObjectUtils.defaultIfNull(userName, defaultUsername);
-        this.userPassword = ObjectUtils.defaultIfNull(userPassword, defaultPassword);
-        this.rootPassword = ObjectUtils.defaultIfNull(rootPassword, defaultPassword);
-    }
+    private String dockerImage = "mariadb:10.6.3-focal";
+    private String databaseName = "test";
+    private String userName = MARIADB;
+    private String userPassword = MARIADB;
+    private String rootPassword = MARIADB;
 }
